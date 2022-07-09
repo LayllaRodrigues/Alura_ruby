74abb5bab43c2356ce7157e1963a294d483fcf26
Laylla Rodrigues <62724150+LayllaRodrigues@users.noreply.github.com>
2022-07-09 16:40:15 -0300

    condicionais

--- game.rb
+++ game.rb
@@ -1,7 +1,7 @@
 puts "Bem vindo ao jogo da adivinhação"
 puts "Qual é o seu nome?"
 
-nome = gets.chomp 
+nome = gets
 
 puts " Começaremos o jogo para você, " + nome + "!"
 puts "\n\n"
@@ -10,16 +10,17 @@ puts "Escolhendo um numero entre 0 e 200 ..."
 numero_secreto = rand 0..200
 puts "Escolhido... agora tente adivinhar!"
 
-puts "\n\n\n\n"
+puts "\n\n\n"
 
+puts "Tentativa 1"
 puts "Entre com o numero"
-chute = gets.chomp
+chute = gets
 
 puts "Será que acertou? Seu chute foi: " + chute
 
-if chute == numero_secreto 
-    puts "Boa! Vc acertou!"
-elsif
+if numero_secreto == chute.to_i
+    puts "Boa, vc acertou!"
+else
     puts "Ops, não foi dessa vez! O numero secreto era " + numero_secreto.to_s
 end
 
