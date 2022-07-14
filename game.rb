081964ecf08698f71283f452a74145bd8648d8cc
Laylla Rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2022-07-14 18:46:18 -0300

    adc funcao boas vindas

--- game.rb
+++ game.rb
@@ -1,14 +1,21 @@
-puts "Bem vindo ao jogo da adivinhação"
-puts "Qual é o seu nome?"
+def da_boas_vindas 
+    puts "Bem vindo ao jogo da adivinhação"
+    puts "Qual é o seu nome?"
+    nome = gets
+    puts "\n\n\n"
+    puts " Começaremos o jogo para você, " + nome 
+end
 
-nome = gets
+# def sorteia_numero_secreto
+#     puts "\n\n\n"
+#     puts "Escolhendo um numero entre 0 e 200 ..."
+#     @numero_secreto = rand 0..200
+#     puts "Escolhido... agora tente adivinhar!"
+# end
 
-puts " Começaremos o jogo para você, " + nome + "!"
-puts "\n\n"
+da_boas_vindas
+sorteia_numero_secreto
 
-puts "Escolhendo um numero entre 0 e 200 ..."
-numero_secreto = rand 0..200
-puts "Escolhido... agora tente adivinhar!"
 
 limite_de_tentativas = 5
 
@@ -20,13 +27,13 @@ for tentativa in 1..limite_de_tentativas
 
     puts "Será que acertou? Seu chute foi: " + chute
 
-    acertou = numero_secreto == chute.to_i 
+    acertou = @numero_secreto == chute.to_i 
 
     if acertou
         puts "Boa, vc acertou!" 
         break
     else
-        maior = numero_secreto > chute.to_i 
+        maior = @numero_secreto > chute.to_i 
         if maior
             puts "O número secreto é maior!"
         else
