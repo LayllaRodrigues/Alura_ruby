a3097193c954c37349f9f0839f2e570fc819593c
Laylla Rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2022-07-14 18:49:27 -0300

    adc escopo da variavel numero secreto

--- game.rb
+++ game.rb
@@ -9,12 +9,13 @@ end
 def sorteia_numero_secreto
     puts "\n\n\n"
     puts "Escolhendo um numero entre 0 e 200 ..."
-    @numero_secreto = rand 0..200
+    numero_secreto = rand 0..200
     puts "Escolhido... agora tente adivinhar!"
+    return numero_secreto
 end
 
 da_boas_vindas
-sorteia_numero_secreto
+numero_secreto = sorteia_numero_secreto
 
 
 limite_de_tentativas = 5
@@ -27,13 +28,13 @@ for tentativa in 1..limite_de_tentativas
 
     puts "Será que acertou? Seu chute foi: " + chute
 
-    acertou = @numero_secreto == chute.to_i 
+    acertou = numero_secreto == chute.to_i 
 
     if acertou
         puts "Boa, vc acertou!" 
         break
     else
-        maior = @numero_secreto > chute.to_i 
+        maior = numero_secreto > chute.to_i 
         if maior
             puts "O número secreto é maior!"
         else
